3114b617f6a2c022660b8836473a42b639039d9c
Sreeraj S Kurup <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2026-05-02 02:02:29 +0530

    chore: improve JSON serialization and data sanitization

--- netspecter/utils/output.py
+++ netspecter/utils/output.py
@@ -5,14 +5,32 @@ from rich.console import Console
 from rich.pretty import Pretty
 import sys
 import json
+from datetime import datetime, date
 
 console = Console()
 
+def sanitize(obj):
+    if isinstance(obj, dict):
+        return {k: sanitize(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [sanitize(i) for i in obj]
+    elif isinstance(obj, tuple):
+        return [sanitize(i) for i in obj]
+    elif isinstance(obj, bytes):
+        return obj.decode(errors="ignore")
+    elif isinstance(obj, set):
+        return sorted([sanitize(i) for i in obj])
+    elif isinstance(obj,(datetime, date)):
+        return obj.isoformat()
+    else:
+        return obj
+
 def show(data: dict[str, object], as_json: bool = False) -> None:
     if as_json:
+        safe_data = sanitize(data)
         if sys.stdout.isatty():
-            console.print_json(data=data, indent=2, sort_keys=True)
+            console.print_json(data=safe_data, indent=2, sort_keys=True)
         else:
-            print(json.dumps(data, indent=2, sort_keys=True, default=str))
+            print(json.dumps(safe_data, indent=2, sort_keys=True))
     else:
         console.print(Pretty(data))
